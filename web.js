326b37340c4689848df7648d4b15ee4af5ef3f55
Yaroslav Grishajev <ygrishajev@gmail.com>
2020-11-05 21:53:44 +0200

    feat: redirect all methods

--- web.js
+++ web.js
@@ -9,6 +9,18 @@ app.get('*', function(request, response) {
   response.redirect(redirectStatus, newBaseURL + request.url);
 });
 
+app.post('*', function(request, response) {
+  response.redirect(redirectStatus, newBaseURL + request.url);
+});
+
+app.put('*', function(request, response) {
+  response.redirect(redirectStatus, newBaseURL + request.url);
+});
+
+app.delete('*', function(request, response) {
+  response.redirect(redirectStatus, newBaseURL + request.url);
+});
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
